ea7e210d67c13455b915c8e902a9843e572e4317
Oleg Mind <oleg22597@mail.ru>
2022-09-12 17:14:04 +0300

    add slider pagination

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
-const burgerMenuList = document.querySelector(".burger-menu");
-const burgerBg = document.querySelector(".burger-menu__bg");
+const burgerMenuList = document.querySelector('.burger-menu');
+const burgerBg = document.querySelector('.burger-menu__bg');
 
-const menuBurgerBtn = document.querySelector(".menu-button");
-const menuBurgerInnerBtn = document.querySelector(".inner-button");
-menuBurgerBtn.addEventListener("click", () => {
-    menuBurgerBtn.classList.toggle("menu-button--active");
-    burgerMenuList.classList.toggle("burger-menu--active");
-    burgerBg.classList.toggle("bg-lock");
+const menuBurgerBtn = document.querySelector('.menu-button');
+const menuBurgerInnerBtn = document.querySelector('.inner-button');
+menuBurgerBtn.addEventListener('click', () => {
+  menuBurgerBtn.classList.toggle('menu-button--active');
+  burgerMenuList.classList.toggle('burger-menu--active');
+  burgerBg.classList.toggle('bg-lock');
 });
 
-burgerBg.addEventListener("click", () => {
-    menuBurgerBtn.classList.toggle("menu-button--active");
-    burgerMenuList.classList.toggle("burger-menu--active");
-    burgerBg.classList.toggle("bg-lock");
+burgerBg.addEventListener('click', () => {
+  menuBurgerBtn.classList.toggle('menu-button--active');
+  burgerMenuList.classList.toggle('burger-menu--active');
+  burgerBg.classList.toggle('bg-lock');
 });
 
-menuBurgerInnerBtn.addEventListener("click", () => {
-  menuBurgerBtn.classList.remove("menu-button--active");
-  burgerMenuList.classList.remove("burger-menu--active");
-  burgerBg.classList.remove("bg-lock");
+menuBurgerInnerBtn.addEventListener('click', () => {
+  menuBurgerBtn.classList.remove('menu-button--active');
+  burgerMenuList.classList.remove('burger-menu--active');
+  burgerBg.classList.remove('bg-lock');
 });
-
 
 // first slider
 const sliderArrows = document.querySelector('.slider-arrows');
@@ -36,20 +35,18 @@ sliderArrowsPrev.addEventListener('click', prevSlide);
 hideSlides();
 showSlide();
 
-
 function nextSlide(index) {
   sliderArrowsIndex += sliderArrowStep;
-  if(sliderArrowsIndex > sliderArrowsItems.length - 1 ) {
+  if (sliderArrowsIndex > sliderArrowsItems.length - 1) {
     sliderArrowsIndex = 0;
   }
-  hideSlides(); 
+  hideSlides();
   showSlide();
-  
 }
 
 function prevSlide(index) {
   sliderArrowsIndex -= sliderArrowStep;
-  if(sliderArrowsIndex < 0) {
+  if (sliderArrowsIndex < 0) {
     sliderArrowsIndex = sliderArrowsItems.length - 1;
   }
   hideSlides();
@@ -57,8 +54,56 @@ function prevSlide(index) {
 }
 
 function hideSlides() {
-  sliderArrowsItems.forEach((item, index) => item.style.display = 'none');
+  sliderArrowsItems.forEach((item, index) => (item.style.display = 'none'));
 }
 function showSlide() {
   sliderArrowsItems[sliderArrowsIndex].style.display = 'block';
 }
+
+//second slider with dots
+const sliderDots = document.querySelector('.slider-dots');
+const sliderDotsWrapper = sliderDots.querySelector('.slider-dots__nav');
+const sliderDotsItemsImg = document.querySelectorAll('.slider-dots__item');
+
+let sliderDotsIndex = 0;
+
+function renderPagin() {
+  for (let i = 0; i < sliderDotsItemsImg.length; i++) {
+    if (i === 0) {
+      sliderDotsWrapper.insertAdjacentHTML(
+        'beforeend',
+        `<button class="slider-dots__nav-item slider-dots__nav-item--active" data-index="0"></button>`
+      );
+    } else {
+      const sliderDotsHtmlItem = `<button class="slider-dots__nav-item" data-index="${i}"></button>`;
+      sliderDotsWrapper.insertAdjacentHTML('beforeend', sliderDotsHtmlItem);
+    }
+  }
+}
+function changeImg() {
+  sliderDotsItemsImg.forEach((item, index) => {
+    item.classList.remove('item--active');
+  });
+
+  sliderDotsItemsImg[sliderDotsIndex].classList.add('item--active');
+}
+
+sliderDotsItemsImg.forEach((item, index) => {
+  if (index === 0) {
+    item.classList.add('item--active');
+  }
+});
+renderPagin();
+
+const sliderDotsItems = sliderDots.querySelectorAll('.slider-dots__nav-item');
+
+sliderDotsItems.forEach((item) => {
+  item.addEventListener('click', function () {
+    sliderDotsItems.forEach((item) => {
+      item.classList.remove('slider-dots__nav-item--active');
+    });
+    this.classList.add('slider-dots__nav-item--active');
+    sliderDotsIndex = Number(this.dataset.index);
+    changeImg();
+  });
+});
